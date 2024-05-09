bd65286fdc06bdebc5775b4b1372660833a5b711
Gustavo Sanchez <gussan2127@gmail.com>
2024-05-09 17:56:12 -0300

    DESAFIO

--- # 1️⃣ Desafio Classificador de nível de Herói/Desafio.js
+++ # 1️⃣ Desafio Classificador de nível de Herói/Desafio.js
@@ -1,23 +1,25 @@
 let nomeHeroi = "Homem de Ferro";
-let xpHeroi = 5000;
+let xpHeroi = 15000;
 let nivelHeroi;
 
 if (xpHeroi <= 1000) {
     nivelHeroi = "Ferro";
-} else if (xpHeroi <= 2000) {
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
     nivelHeroi = "Bronze";
-} else if (xpHeroi <= 5000) {
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
     nivelHeroi = "Prata";
-} else if (xpHeroi <= 7000) {
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
     nivelHeroi = "Ouro";
-} else if (xpHeroi <= 8000) {
+} else if (xpHeroi >= 1001 && xpHeroi <= 8000) {
     nivelHeroi = "Platina";
-} else if (xpHeroi <= 9000) {
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
     nivelHeroi = "Ascendente";
-} else if (xpHeroi <= 10000) {
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
     nivelHeroi = "Imortal";
-} else {
+} else if(xpHeroi >= 10000){
     nivelHeroi = "Radiante";
+}else{
+    console.log("NOOB");
 }
 
 console.log("O Herói de Nome " + nomeHeroi + " está no Nível de " + nivelHeroi);
